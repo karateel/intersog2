6d1a7df55acdb09e5bf93b9d71be17e5ea001f64
kyurov <karateel@users.noreply.github.com>
2021-09-13 14:49:20 +0300

    i hate youtube

--- app/index.js
+++ app/index.js
@@ -7,55 +7,50 @@ const searchResults = '30'
 
 const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${searchResults}&q=${search}&key=${key}`
 
-const getDataBtn = document.getElementById('getData')
+
+function createBtn(className) {
+  const btn = document.createElement("button");
+  btn.className = className;
+  btn.innerText = "Get data";
+  return btn;
+}
+
+function createList(className) {
+  const list = document.createElement("ul");
+  list.className = className;
+  return list;
+}
+
+const getDataBtn = createBtn("btn");
+document.body.appendChild(getDataBtn);
+document.body.appendChild(createList("list"));
 
 getDataBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  getData("GET", url , onLoad, onError);
+  event.target.innerText = "getting data...";
+  getData(url)
+    .then(response => onLoad(response))
+    .catch(error => console.error(error));
 })
 
-function getData(request, url, callback, onerror) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(request, url);
-  xhr.responseType = "json";
-  xhr.send();
-  xhr.onload = () => {
-    if (xhr.status !== 200) {
-      onerror(xhr);
-      return;
-    }
-    callback(xhr.response);
-  }
-  xhr.onerror = () => {
-    onerror(xhr);
-  }
+function getData() {
+  return fetch(url)
+    .then(res => res.json());
 }
 
 function onLoad(data) {
-  if (data.info === null) return
-  const list = document.getElementById('list');
-  for (let i of Object.entries(data)) {
-    for(let j of i.values()){
-      console.log(j)
+  const btn = document.querySelector("button");
+  if (data.info === null) {
+    btn.innerText = "end";
+    return;
+  }
+  btn.innerText = "Get data";
+  btn.disabled = false;
+  const list = document.querySelector("ul");
+  list.innerHTML = "";
+  let dataValues = Object.keys(data)
+  for (let i of dataValues[5]) {
       const li = document.createElement("li");
-      li.innerText = j[6]
+      li.innerText = i;
       list.appendChild(li);
-    }
-
-    // if(data[i] === "object"){
-    //   onLoad(data[i].items)
-    // } else {
-    // }
   }
 }
-function onError(xhr) {
-  console.error(new Error(`Error ${xhr.status}`));
-}
-
-
-// if(data.hasOwnProperty(i)) {
-//   console.log(data)
-//   console.log(i)
-//   console.log()
-// }
